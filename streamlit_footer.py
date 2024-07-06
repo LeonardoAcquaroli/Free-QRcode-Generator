8f2d957142e37d3ca393cf67453452c8911379f6
Leonardo Acquaroli <leoacqua2010.la@gmail.com>
2024-07-06 15:24:13 +0200

    emoji

--- streamlit_footer.py
+++ streamlit_footer.py
@@ -4,6 +4,7 @@ class PersonalFooter:
     def __init__(self,
                  line_separator=True,
                  author='Leonardo Acquaroli',
+                 emoji='👌🏻',
                  image_url='https://raw.githubusercontent.com/LeonardoAcquaroli/Free-QRcode-Generator/master/Leo_wood-circlecrop.png',
                  linkedin_url='https://www.linkedin.com/in/leonardo-acquaroli/',
                  github_url='https://github.com/LeonardoAcquaroli',
@@ -18,7 +19,7 @@ class PersonalFooter:
         # Adjusted footer content with streamlit elements
         footer_col1, footer_col2 = st.columns(2)
         with footer_col1:
-            st.markdown(f"Brought to you by **{author}**")
+            st.markdown(f"Brought to you by **{author}**{emoji if emoji else ''}")
         with footer_col2:
             st.image(image_url, width=image_width)
             st.markdown(f"[LinkedIn]({linkedin_url}) | [GitHub]({github_url})")
